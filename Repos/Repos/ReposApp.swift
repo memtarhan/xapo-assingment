3427ffd236a6d6639e2575fdc19483d83bd67cfc
Mehmet Tarhan <hi@memtarhan.com>
2023-05-03 00:15:11 +0100

    <dev>: create xcode project

---
+++ Repos/Repos/ReposApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReposApp.swift
+//  Repos
+//
+//  Created by Mehmet Tarhan on 03/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct ReposApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
